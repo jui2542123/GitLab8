070031137890f4f91cdae3191c4eac84a831dc7c
VarutAunvilai <55436783+VarutAunvilai@users.noreply.github.com>
2020-03-12 18:01:17 +0700

    wertghy

--- program8.py
+++ program8.py
@@ -8,7 +8,7 @@ class Simple_drawing_window(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Drawing")
-        self.rabbit = QPixmap("images/rabbit.png")
+        self.rabbit = QPixmap("images/Derp.png")
 
     def paintEvent (self, e):
         p = QPainter()
